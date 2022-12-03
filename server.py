6f79aaf4883077755ce447ebcf307cb8c55a493f
elayemgee <65518364+elayemgee@users.noreply.github.com>
2022-12-04 00:52:40 +0800

    input text

--- server.py
+++ server.py
@@ -6,5 +6,4 @@ sock.bind(('127.0.0.1', 12345))
 while True:
     data,addr = sock.recvfrom(4096)
     print(str(data))
-    message=bytes('Hello I am UDP Server', encoding='utf-8')
-    sock.sendto(message,addr)
+    sock.sendto(data,addr)
