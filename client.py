0b29f8f26eb7ad770dd3088f658d80b8c9722d8d
elayemgee <65518364+elayemgee@users.noreply.github.com>
2022-12-03 23:42:15 +0800

    Notes

--- client.py
+++ client.py
@@ -1,1 +1,9 @@
 import socket
+client_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+msg="Hello UDP Server"
+client_socket.sendto(msg.encode("utf-8"),('127.0.0.1', 12345))
+data,addr=client_socket.recvfrom(4096)
+print("Server Says")
+print(str)
+client_socket.close()
--- server.py
+++ server.py
@@ -1,1 +1,10 @@
 import socket
+
+sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('127.0.0.1', 12345))
+
+while True:
+    data,addr = sock.recvfrom(4096)
+    print(str(data))
+    message=bytes('Hello I am UDP Server', encoding='utf-8')
+    sock.sendto(message,addr)
