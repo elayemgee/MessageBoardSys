b2f4c87538dc643fab6a4fd2d643b00eba9650b1
elayemgee <65518364+elayemgee@users.noreply.github.com>
2022-12-04 08:52:09 +0800

    small changes
    import json
    rename variables
    notes

--- client.py
+++ client.py
@@ -1,9 +1,11 @@
 import socket
-client_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+import json
+
+clientsock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 msg=input()
-client_socket.sendto(msg.encode("utf-8"),('127.0.0.1', 12345))
-data,addr=client_socket.recvfrom(4096)
+clientsock.sendto(msg.encode("utf-8"),('127.0.0.1', 12345))
+data,addr=clientsock.recvfrom(4096)
 print("Server Says")
 print(str(msg))
-client_socket.close()
+clientsock.close()
--- server.py
+++ server.py
@@ -1,9 +1,34 @@
 import socket
+import json
 
-sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('127.0.0.1', 12345))
+serversock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serversock.bind(('127.0.0.1', 12345))
 
 while True:
-    data,addr = sock.recvfrom(4096)
+    data,addr = serversock.recvfrom(4096)
     print(str(data))
-    sock.sendto(data,addr)
+    serversock.sendto(data,addr)
+
+
+""" NOTES
+import socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+host = input("Enter The Server Ip: ")
+port = input("Enter The Server Port: ")
+
+serversocket.bind((host, port)) 
+
+serversocket.listen(10)
+
+while True :
+    clientsocket, address = serversocket.accept()
+
+    print("Received Connection From %s" % str(address))
+
+    message = 'Connection Established' + "\r\n"
+    clientsocket.send(message.encode("ascii"))
+
+    clientsocket.close()
+    """
